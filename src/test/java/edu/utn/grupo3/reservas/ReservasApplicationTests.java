e6b4728d1e05e9ffdb5a6ab53732302c6033226f
LucasOrtiz5 <teiteortiz@gmail.com>
2023-04-18 19:04:22 -0300

    first commit

---
+++ src/test/java/edu/utn/grupo3/reservas/ReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utn.grupo3.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
