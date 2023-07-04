1f9e193e9a648d5f55ec54703b10a1471d33a3d7
LucasOrtiz5 <teiteortiz@gmail.com>
2023-07-04 19:12:48 -0300

    arreglando side-bar

--- reservas-frontend/src/app/app.module.ts
+++ reservas-frontend/src/app/app.module.ts
@@ -12,6 +12,11 @@ import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatSelectModule} from '@angular/material/select';
 import { MatInputModule} from '@angular/material/input';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { ListaSolicitantesComponent } from './lista-solicitantes/lista-solicitantes.component';
@@ -40,6 +45,11 @@ import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
     MatDatepickerModule,
     MatNativeDateModule,
     NgIf,
